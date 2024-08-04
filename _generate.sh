c93465b78c3bb3b4eaf3269ea502f37938ca21d4
Marco Köpcke <hello@capypara.de>
2024-08-04 16:49:11 +0200

    New Release 1.8.0 (#28)
    Co-authored-by: theCapypara <3512122+theCapypara@users.noreply.github.com>

--- _generate.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -x
-# This script generates the depdendant JSON files.
-# The environment variables SKYTEMPLE_DIR SKYTEMPLE_RUST_DIR, SKYTEMPLE_SSB_EMULATOR_DIR must be set to the correct paths
-# of SkyTemple and skytemple-rust in matching versions.
-python3 flatpak-builder-tools/pip/flatpak-pip-generator -r $SKYTEMPLE_DIR/requirements-mac-windows.txt -o requirements-skytemple
-python3 flatpak-builder-tools/cargo/flatpak-cargo-generator.py $SKYTEMPLE_RUST_DIR/Cargo.lock -o cargo-sources.json
-python3 flatpak-builder-tools/cargo/flatpak-cargo-generator.py $SKYTEMPLE_SSB_EMULATOR_DIR/Cargo.lock -o cargo-sources-ssb-emulator.json
